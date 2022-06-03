196e9d716c4bfb24653a51d4ff250f90eb2e8385
vitask <skalenko.v@yandex.ru>
2022-06-04 00:37:21 +0700

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Vitalya Skalenko");
+        System.out.println("13.10.1999");
+    }
+}
