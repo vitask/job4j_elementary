3adfe0bc0167c5405d59e6f4bf2beb5c081a9174
vitask <skalenko.v@yandex.ru>
2022-06-07 17:01:14 +0700

    4.3.4. if c return [#333785]

---
+++ src/main/java/ru/job4j/condition/IfElseReturn.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class IfElseReturn {
+
+    public static boolean greatThen(int first, int second) {
+        return first > second;
+    }
+
+    public static void main(String[] args) {
+        boolean result = greatThen(30, 20);
+        System.out.println(result);
+    }
+}
