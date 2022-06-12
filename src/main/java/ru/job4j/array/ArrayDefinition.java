f86bb4777972ce0a86a766311f88f3aeb0c2cc43
vitask <skalenko.v@yandex.ru>
2022-06-12 21:58:39 +0700

    6.0.1.1. Размер массива [#333582]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,9 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surname = new String[100500];
         float[] prices = new float[40];
+
+        System.out.println("Размер массива ages равен: " + ages.length);
+        System.out.println("Размер массива surname равен: " + surname.length);
+        System.out.println("Размер массива prices равен: " + prices.length);
     }
 }
