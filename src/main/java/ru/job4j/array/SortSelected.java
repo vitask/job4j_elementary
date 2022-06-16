5d8351fb81b9645b9a1cd5baab33a7851ff684e3
vitask <skalenko.v@yandex.ru>
2022-06-16 09:22:09 +0700

    6.5. Поиск индекса в диапазоне. [#156318 #312679]

--- src/main/java/ru/job4j/array/SortSelected.java
+++ src/main/java/ru/job4j/array/SortSelected.java
@@ -4,7 +4,7 @@ public class SortSelected {
     public static int[] sort(int[] data) {
         for (int i = 0; i < data.length; i++) {
             int min = MinDiapason.findMin(data, i, data.length - 1);
-            int index = FindLoop.indexOfRange(data, min, i, data[data.length - 1]);
+            int index = FindLoop.indexOfRange(data, min, i, data.length - 1);
             SwitchArray.swap(data, i, index);
         }
         return data;
