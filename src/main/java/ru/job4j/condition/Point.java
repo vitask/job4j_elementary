b70a11031b13a01f422c4e576625cf2eb4bb8cf7
vitask <skalenko.v@yandex.ru>
2022-06-19 16:58:26 +0700

    4. Расстояние между точками в трехмерном пространстве. [#122643]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -7,20 +7,37 @@ public class Point {
 
     private int x;
     private int y;
+    private int z;
 
     public Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
     public double distance(Point that) {
         return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
     }
 
+    public double distance3d(Point that) {
+        return Math.sqrt(pow(this.x - that.x, 2)
+                + pow(this.y - that.y, 2)
+                + pow(this.z - that.z, 2));
+    }
+
+    public void info() {
+        System.out.println("Point[" + this.x + "," + this.y + "]");
+    }
+
     public static void main(String[] args) {
-        Point a = new Point(0, 0);
-        Point b = new Point(0, 2);
-        double distance = a.distance(b);
-        System.out.println(distance);
+        Point a = new Point(4, 0, 2);
+        Point b = new Point(2, 0, 2);
+        double dist3 = a.distance3d(b);
+        System.out.println(dist3);
     }
 }
--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -40,4 +40,22 @@ public class PointTest {
         double expected = 5;
         Assert.assertEquals(expected, out, 0.01);
     }
+
+    @Test
+    public void when402To202Then2() {
+        Point a = new Point(4, 0, 2);
+        Point b = new Point(2, 0, 2);
+        double out = a.distance3d(b);
+        double expected = 2;
+        Assert.assertEquals(expected, out, 0.001);
+    }
+
+    @Test
+    public void when777To555Then3dot464() {
+        Point a = new Point(7, 7, 7);
+        Point b = new Point(5, 5, 5);
+        double out = a.distance3d(b);
+        double expected = 3.464;
+        Assert.assertEquals(expected, out, 0.001);
+    }
 }
